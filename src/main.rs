997f3a58ad68f1d0061ed0afe89ff11746996f33
Kyle Rosenberg <kyle@ekrosenberg.com>
2023-11-08 18:35:00 -0500

    allow generating many keys at once

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use core::panic;
+use std::iter::FromIterator;
 
 use anyhow::Ok;
 use clap::{Parser, Subcommand};
@@ -18,7 +19,10 @@ struct Cli {
 
 #[derive(Debug, Subcommand)]
 enum Command {
-    KeyPair,
+    KeyPair {
+        #[arg(short, long, default_value_t = 1)]
+        count: usize,
+    },
     Encrypt {
         my_private_key: String,
         their_public_key: String,
@@ -31,22 +35,26 @@ enum Command {
     },
 }
 
-fn generate_key_pair() -> anyhow::Result<()> {
+fn generate_key_pair(count: usize) -> anyhow::Result<()> {
     let curve = EllipticalCurve::secp256k1();
 
-    let private_key = curve.gen_private_key();
+    let mut privates = vec![];
+    let mut publics = vec![];
 
-    let uncompressed_public_key = match curve.nth_point(&private_key) {
-        EllipticalPoint::Value(p) => p,
-        EllipticalPoint::Identity => panic!("The identity is not useful"),
-    };
+    for _ in 0..count {
+        let private_key = curve.gen_private_key();
+        privates.push(hex::encode(private_key.to_bytes_be()));
 
-    println!("private key {:?}", hex::encode(private_key.to_bytes_be()));
-    println!(
-        "public key {:?}",
-        hex::encode(uncompressed_public_key.compress().to_bytes())
-    );
+        let uncompressed_public_key = match curve.nth_point(&private_key) {
+            EllipticalPoint::Value(p) => p,
+            EllipticalPoint::Identity => panic!("The identity is not useful"),
+        };
 
+        publics.push(hex::encode(uncompressed_public_key.compress().to_bytes()));
+    }
+
+    println!("private {:?}", privates);
+    println!("public {:?}", publics);
     Ok(())
 }
 
@@ -70,25 +78,33 @@ fn shared_secret(my_private_key: &str, their_public_key: &str) -> anyhow::Result
     Ok(sha_512(&shared_point_bytes))
 }
 
+const PREFIX: &[u8] = b"prefix's are fun";
+
 fn encrypt(my_private_key: &str, their_public_key: &str, plaintext: &str) -> anyhow::Result<()> {
     let shared_secret = shared_secret(my_private_key, their_public_key)?;
 
-    if plaintext.bytes().len() > 64 {
-        panic!("can not encode message longer than 64 bytes");
-    }
-
     let mut message_bytes = plaintext.as_bytes().to_vec();
-    while message_bytes.len() < 64 {
-        message_bytes.push(rand::thread_rng().gen_range(97..123));
+    while message_bytes.len() % 64 != 0 {
+        message_bytes.push(rand::thread_rng().gen_range(97..123))
     }
 
-    let ciphertext = shared_secret
-        .iter()
-        .zip(message_bytes)
-        .map(|(a, b)| a ^ b)
-        .collect::<Box<_>>();
+    let mut key_stream = shared_secret;
+    let mut cipher_text: Vec<u8> = vec![];
+    for chunk in message_bytes.chunks_exact(64) {
+        let mut prefix = PREFIX.to_vec();
+        prefix.extend(key_stream.iter());
+        key_stream = sha_512(&prefix);
 
-    println!("cipher text {}", hex::encode(ciphertext));
+        let c = key_stream
+            .iter()
+            .zip(chunk)
+            .map(|(a, b)| a ^ b)
+            .collect::<Box<_>>();
+
+        cipher_text.extend(c.into_iter());
+    }
+
+    println!("cipher text: {}", hex::encode(cipher_text));
 
     Ok(())
 }
@@ -97,17 +113,27 @@ fn decrypt(my_private_key: &str, their_public_key: &str, ciphertext: &str) -> an
     let shared_secret = shared_secret(my_private_key, their_public_key)?;
     let ciphertext_bytes = hex::decode(ciphertext)?;
 
-    if ciphertext_bytes.len() < 64 {
-        panic!("message must be 64 bytes");
+    if ciphertext_bytes.len() % 64 != 0 {
+        panic!("message must be multiple of 64 bytes");
     }
 
-    let plaintext = shared_secret
-        .iter()
-        .zip(ciphertext_bytes)
-        .map(|(a, b)| (a ^ b) as char)
-        .collect::<String>();
+    let mut key_stream = shared_secret;
+    let mut plain_text: Vec<char> = vec![];
+    for chunk in ciphertext_bytes.chunks_exact(64) {
+        let mut prefix = PREFIX.to_vec();
+        prefix.extend(key_stream.iter());
+        key_stream = sha_512(&prefix);
 
-    println!("plaintext {}", plaintext);
+        let c = key_stream
+            .iter()
+            .zip(chunk)
+            .map(|(a, b)| (a ^ b) as char)
+            .collect::<Box<_>>();
+
+        plain_text.extend(c.into_iter());
+    }
+
+    println!("plain text: {}", String::from_iter(plain_text));
 
     Ok(())
 }
@@ -116,7 +142,7 @@ pub fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
     match cli.command {
-        Command::KeyPair => generate_key_pair(),
+        Command::KeyPair { count } => generate_key_pair(count),
         Command::Encrypt {
             my_private_key,
             their_public_key,
