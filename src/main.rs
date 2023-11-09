851e090e7ca0ef458af7983ecea6cdeb9fbe41f0
Kyle Rosenberg <kyle@ekrosenberg.com>
2023-11-08 19:03:48 -0500

    change args to flags

--- src/main.rs
+++ src/main.rs
@@ -24,14 +24,22 @@ enum Command {
         count: usize,
     },
     Encrypt {
+        #[arg(short, long)]
         my_private_key: String,
+        #[arg(short, long)]
         their_public_key: String,
-        plaintext: String,
+        #[arg(short, long)]
+        plain_text: String,
     },
     Decrypt {
+        #[arg(short, long)]
         my_private_key: String,
+        #[arg(short, long)]
         their_public_key: String,
-        ciphertext: String,
+        #[arg(short, long)]
+        cipher_text: String,
+        #[arg(short, long)]
+        nonce: String,
     },
 }
 
@@ -79,16 +87,21 @@ fn shared_secret(my_private_key: &str, their_public_key: &str) -> anyhow::Result
 }
 
 const PREFIX: &[u8] = b"prefix's are fun";
+const NONCE_LEN: usize = 64;
 
 fn encrypt(my_private_key: &str, their_public_key: &str, plaintext: &str) -> anyhow::Result<()> {
     let shared_secret = shared_secret(my_private_key, their_public_key)?;
+    let mut nonce = [0; NONCE_LEN];
+    rand::thread_rng().fill(&mut nonce);
 
     let mut message_bytes = plaintext.as_bytes().to_vec();
     while message_bytes.len() % 64 != 0 {
         message_bytes.push(rand::thread_rng().gen_range(97..123))
     }
 
-    let mut key_stream = shared_secret;
+    let mut nonce_stream = nonce.to_vec();
+    nonce_stream.extend(shared_secret.iter());
+    let mut key_stream: Box<[u8]> = nonce_stream.into();
     let mut cipher_text: Vec<u8> = vec![];
     for chunk in message_bytes.chunks_exact(64) {
         let mut prefix = PREFIX.to_vec();
@@ -105,19 +118,31 @@ fn encrypt(my_private_key: &str, their_public_key: &str, plaintext: &str) -> any
     }
 
     println!("cipher text: {}", hex::encode(cipher_text));
+    println!("nonce: {}", hex::encode(nonce));
 
     Ok(())
 }
 
-fn decrypt(my_private_key: &str, their_public_key: &str, ciphertext: &str) -> anyhow::Result<()> {
+fn decrypt(
+    my_private_key: &str,
+    their_public_key: &str,
+    ciphertext: &str,
+    nonce: &str,
+) -> anyhow::Result<()> {
     let shared_secret = shared_secret(my_private_key, their_public_key)?;
     let ciphertext_bytes = hex::decode(ciphertext)?;
+    let nonce = hex::decode(nonce)?;
+    if nonce.len() != NONCE_LEN {
+        panic!("nonce is not the write length");
+    }
 
     if ciphertext_bytes.len() % 64 != 0 {
         panic!("message must be multiple of 64 bytes");
     }
 
-    let mut key_stream = shared_secret;
+    let mut nonce_stream = nonce.to_vec();
+    nonce_stream.extend(shared_secret.iter());
+    let mut key_stream: Box<[u8]> = nonce_stream.into();
     let mut plain_text: Vec<char> = vec![];
     for chunk in ciphertext_bytes.chunks_exact(64) {
         let mut prefix = PREFIX.to_vec();
@@ -146,12 +171,13 @@ pub fn main() -> anyhow::Result<()> {
         Command::Encrypt {
             my_private_key,
             their_public_key,
-            plaintext,
-        } => encrypt(&my_private_key, &their_public_key, &plaintext),
+            plain_text,
+        } => encrypt(&my_private_key, &their_public_key, &plain_text),
         Command::Decrypt {
             my_private_key,
             their_public_key,
-            ciphertext,
-        } => decrypt(&my_private_key, &their_public_key, &ciphertext),
+            cipher_text,
+            nonce,
+        } => decrypt(&my_private_key, &their_public_key, &cipher_text, &nonce),
     }
 }
